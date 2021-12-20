438973f4670af3867bcaf5df6ceae3b41b791b67
JSerranoP <jserranop@minsait.com>
2021-12-20 12:33:04 +0100

    Añadido hover y comienzo navegacion entre paginas

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,77 +1,114 @@
 window.onload = function() {
     //setTimeout(showAlert, 2000);
     showMain();
+    addListeners();
 }
+
+var current;
 
 function showAlert() {
     alert("Bienvenido a mi web");
 }
 
-function showList() {
+function addListeners() {
+    document.getElementById("home__icon").addEventListener("click", showMain);
+    document.getElementById("home__button").addEventListener("click", showMain);
+    document.getElementById("list__button").addEventListener("click", showList);
+    document.getElementById("home__icon").addEventListener("mouseover", onMouseOver);
+    document.getElementById("home__button").addEventListener("mouseover", onMouseOver);
+    document.getElementById("list__button").addEventListener("mouseover", onMouseOver);
+    document.getElementById("home__icon").addEventListener("mouseout", onMouseOut);
+    document.getElementById("home__button").addEventListener("mouseout", onMouseOut);
+    document.getElementById("list__button").addEventListener("mouseout", onMouseOut);
+}
 
+var onMouseOver = function(event) {
+    document.getElementById(event.target.id).classList.add("header__navbarItem--hover");
+}
+
+var onMouseOut = function(event) {
+    document.getElementById(event.target.id).classList.remove("header__navbarItem--hover");
+}
+
+var showList = function(event) {
+    current = 1
+    var padre = document.getElementById("main");
+    var child = document.getElementById("main__content");
+    padre.removeChild(child);
+    console.log(child);
+    console.log(event);
 }
 
 function showMain() {
-    const contenedor = document.getElementById('main');
+    if(current == 0){
+        alert("Ya estamos aquí");
+    } else {
+        current = 0;
+        const contenedor = document.getElementById('main');
 
-    const titulo = document.createElement('h2');
-    titulo.setAttribute('class', 'main__title');
-    titulo.textContent = "Temática: Pokemon";
+        const main_content = document.createElement('div');
+        main_content.setAttribute('class', 'main__content');
 
-    const parrafo1 = document.createElement('p');
-    parrafo1.setAttribute('class', 'main__content');
-    parrafo1.textContent = "En esta web se pretende mostrar una lista de Pokemons con sus respectivos nombres, imagenes y descripción";
+        const titulo = document.createElement('h2');
+        titulo.setAttribute('class', 'main__title');
+        titulo.textContent = "Temática: Pokemon";
 
-    const parrafo2 = document.createElement('p');
-    parrafo2.setAttribute('class', 'main__content');
-    parrafo2.textContent = "En el header encontramos el link a la vista actual y a la vista del listado de los distintos pokemons";
+        const parrafo1 = document.createElement('p');
+        parrafo1.setAttribute('class', 'main__paragraph');
+        parrafo1.textContent = "En esta web se pretende mostrar una lista de Pokemons con sus respectivos nombres, imagenes y descripción";
 
-    const parrafo3 = document.createElement('p');
-    parrafo3.setAttribute('class', 'main__content');
-    parrafo3.textContent = "En el footer o pie de pagina, podemos ver los iconos con los links a las redes sociales de Pokemon";
+        const parrafo2 = document.createElement('p');
+        parrafo2.setAttribute('class', 'main__paragraph');
+        parrafo2.textContent = "En el header encontramos el link a la vista actual y a la vista del listado de los distintos pokemons";
 
-    const parrafo4 = document.createElement('p');
-    parrafo4.setAttribute('class', 'main__content');
-    parrafo4.textContent = "En la vista principal, al hacer hover por algun elemento, este cambia de color, y al hacer out vuelve a su estado original";
+        const parrafo3 = document.createElement('p');
+        parrafo3.setAttribute('class', 'main__paragraph');
+        parrafo3.textContent = "En el footer o pie de pagina, podemos ver los iconos con los links a las redes sociales de Pokemon";
 
-    const parrafo5 = document.createElement('p');
-    parrafo5.setAttribute('class', 'main__content');
-    parrafo5.textContent = "Al clickar en un bloque que está condensado, este se despliega para mostrar toda la información";
+        const parrafo4 = document.createElement('p');
+        parrafo4.setAttribute('class', 'main__paragraph');
+        parrafo4.textContent = "En la vista principal, al hacer hover por algun elemento, este cambia de color, y al hacer out vuelve a su estado original";
 
-    const parrafo6 = document.createElement('p');
-    parrafo6.setAttribute('class', 'main__content');
-    parrafo6.textContent = "La vista del listado de Pokemons se ha cargado dinámicamente";
+        const parrafo5 = document.createElement('p');
+        parrafo5.setAttribute('class', 'main__paragraph');
+        parrafo5.textContent = "Al clickar en un bloque que está condensado, este se despliega para mostrar toda la información";
 
-    const parrafo7 = document.createElement('p');
-    parrafo7.setAttribute('class', 'main__content');
-    parrafo7.textContent = "Se muestran inicialmente el nombre y la imagen de cada uno";
+        const parrafo6 = document.createElement('p');
+        parrafo6.setAttribute('class', 'main__paragraph');
+        parrafo6.textContent = "La vista del listado de Pokemons se ha cargado dinámicamente";
 
-    const parrafo8 = document.createElement('p');
-    parrafo8.setAttribute('class', 'main__content');
-    parrafo8.textContent = "Al hacer click sobre ellos, navegaremos a la vista en detalle";
+        const parrafo7 = document.createElement('p');
+        parrafo7.setAttribute('class', 'main__paragraph');
+        parrafo7.textContent = "Se muestran inicialmente el nombre y la imagen de cada uno";
 
-    const parrafo9 = document.createElement('p');
-    parrafo9.setAttribute('class', 'main__content');
-    parrafo9.textContent = "La vista en detalle recibe un objeto con los datos del elemento clickado";
+        const parrafo8 = document.createElement('p');
+        parrafo8.setAttribute('class', 'main__paragraph');
+        parrafo8.textContent = "Al hacer click sobre ellos, navegaremos a la vista en detalle";
 
-    const parrafo10 = document.createElement('p');
-    parrafo10.setAttribute('class', 'main__content');
-    parrafo10.textContent = "En ella aparece de nuevo el nombre y la imagen del Pokemon, pero también su descripción";
-    
-    const saltoLinea1 = document.createElement('br');
-    const saltoLinea2 = document.createElement('br');
+        const parrafo9 = document.createElement('p');
+        parrafo9.setAttribute('class', 'main__paragraph');
+        parrafo9.textContent = "La vista en detalle recibe un objeto con los datos del elemento clickado";
 
-    contenedor.appendChild(titulo);
-    contenedor.appendChild(parrafo1);
-    contenedor.appendChild(parrafo2);
-    contenedor.appendChild(parrafo3);
-    contenedor.appendChild(parrafo4);
-    contenedor.appendChild(parrafo5);
-    contenedor.appendChild(saltoLinea1);
-    contenedor.appendChild(parrafo6);
-    contenedor.appendChild(parrafo7);
-    contenedor.appendChild(parrafo8);
-    contenedor.appendChild(saltoLinea2);
-    contenedor.appendChild(parrafo9);
-    contenedor.appendChild(parrafo10);
+        const parrafo10 = document.createElement('p');
+        parrafo10.setAttribute('class', 'main__paragraph');
+        parrafo10.textContent = "En ella aparece de nuevo el nombre y la imagen del Pokemon, pero también su descripción";
+        
+        const saltoLinea1 = document.createElement('br');
+        const saltoLinea2 = document.createElement('br');
+
+        contenedor.appendChild(main_content);
+        main_content.appendChild(titulo);
+        main_content.appendChild(parrafo1);
+        main_content.appendChild(parrafo2);
+        main_content.appendChild(parrafo3);
+        main_content.appendChild(parrafo4);
+        main_content.appendChild(parrafo5);
+        main_content.appendChild(saltoLinea1);
+        main_content.appendChild(parrafo6);
+        main_content.appendChild(parrafo7);
+        main_content.appendChild(parrafo8);
+        main_content.appendChild(saltoLinea2);
+        main_content.appendChild(parrafo9);
+        main_content.appendChild(parrafo10);
+    }
 }
--- JavaScript/js/info.js
+++ JavaScript/js/info.js
@@ -1,5 +1,8 @@
 
 (async function () {
+    const subcontenedor = document.createElement('div');
+    subcontenedor.setAttribute('class', 'list__content');
+
     for (let i = 1; i < 21; i++) {
         const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + i);
         const data = await response.json();
@@ -7,37 +10,31 @@
 
         const contenedor = document.getElementById('main');
 
-        // Creamos una tarjeta
+        
+
         const tarjeta = document.createElement('div');
         tarjeta.setAttribute('class', 'card');
 
-        // Creamos el la cabecera y el cuerpo de la tarjeta
         const cabeceraTarjeta = document.createElement('div');
         cabeceraTarjeta.setAttribute('class', 'card-header');
-
         const cuerpoTarjeta = document.createElement('div');
         cuerpoTarjeta.setAttribute('class', 'card-body');
 
-        // Creamos el encabezado y le asignamos el título de la película
         const titulo = document.createElement('h5');
         titulo.setAttribute('class', 'card-title');
         titulo.textContent = data.name;
-
         const imagen = document.createElement('img');
         imagen.setAttribute('class', 'card-img');
         imagen.setAttribute('src', data.sprites.front_default);
 
-        // Agregamos la tarjeta
-        contenedor.appendChild(tarjeta);
+        contenedor.appendChild(subcontenedor);
+        subcontenedor.appendChild(tarjeta);
 
-        // Agregamos la cabecera y el cuerpo a la tarjeta
         tarjeta.appendChild(cabeceraTarjeta);
         tarjeta.appendChild(cuerpoTarjeta);
 
-        // Agregamos el título a la cabecera
         cabeceraTarjeta.appendChild(titulo);
 
-        // Agregamos la descripción al cuerpo
         cuerpoTarjeta.appendChild(imagen);
     }
 })();
