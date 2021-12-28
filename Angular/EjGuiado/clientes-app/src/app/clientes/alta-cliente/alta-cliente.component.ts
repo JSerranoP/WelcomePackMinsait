0c8bfd4acb42df42fd2d71632ddcdbedc6e2770a
JSerranoP <jserranop@minsait.com>
2021-12-28 12:09:57 +0100

    Completado ejercicio guiado

--- Angular/EjGuiado/clientes-app/src/app/clientes/alta-cliente/alta-cliente.component.ts
+++ Angular/EjGuiado/clientes-app/src/app/clientes/alta-cliente/alta-cliente.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-alta-cliente',
   templateUrl: './alta-cliente.component.html',
-  styleUrls: ['./alta-cliente.component.css']
+  styleUrls: ['./alta-cliente.component.scss']
 })
 export class AltaClienteComponent implements OnInit {
 
--- Angular/EjGuiado/clientes-app/src/app/clientes/listado-clientes/listado-clientes.component.ts
+++ Angular/EjGuiado/clientes-app/src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listado-clientes',
   templateUrl: './listado-clientes.component.html',
-  styleUrls: ['./listado-clientes.component.css']
+  styleUrls: ['./listado-clientes.component.scss']
 })
 export class ListadoClientesComponent implements OnInit {
 
