df04cd018315149a4baf25c5d6dc7a27480fd6b9
JSerranoP <jserranop@minsait.com>
2022-01-21 10:10:46 +0100

    Completado lazy loading

--- Angular/ProyectoFinal/super-heroes-app/src/app/app-routing.module.ts
+++ Angular/ProyectoFinal/super-heroes-app/src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainAboutComponent } from './about/main-about/main-about.component';
-import { MainDetailComponent } from './detail/main-detail/main-detail.component';
-import { MainFormComponent } from './form/main-form/main-form.component';
-import { MainHomeComponent } from './home/main-home/main-home.component';
-import { MainListComponent } from './list/main-list/main-list.component';
-import { MainMyCreatesComponent } from './my-creates/main-my-creates/main-my-creates.component';
-import { UpdateMyCreatesComponent } from './my-creates/update-my-creates/update-my-creates.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: MainHomeComponent },
+  { path: 'home', loadChildren: () => import ('./home/home.module').then (m => m.HomeModule) },
   { path: 'list', loadChildren: () => import ('./list/list.module').then (m => m.ListModule) },
   { path: 'about', loadChildren: () => import ('./about/about.module').then (m => m.AboutModule) },
   { path: 'form', loadChildren: () => import ('./form/form.module').then (m => m.FormModule) },
