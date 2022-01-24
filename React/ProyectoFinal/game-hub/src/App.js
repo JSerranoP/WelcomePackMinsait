8b3ea95f95609bffcc65b62e7611513862767778
JSerranoP <jserranop@minsait.com>
2022-01-24 14:58:22 +0100

    Comienzo routing y diseño

--- React/ProyectoFinal/game-hub/src/App.js
+++ React/ProyectoFinal/game-hub/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import { Counter } from './features/counter/Counter';
+import { Home } from './components/Home/Home';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Counter />
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/counter' element={<Counter/>} />
+        <Route path='/tictactoe' element={<Counter/>} />
+        <Route path='/hangman' element={<Counter/>} />
+        <Route path='/sudoku' element={<Counter/>} />
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ React/ProyectoFinal/game-hub/src/components/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Home.css';
+
+export const Home = () => {
+    return (
+        <div className='home__container'>
+            <h1 className='home__title'>GAME HUB</h1>
+
+            <div className='home__container-games'>
+                <Link className='home__link' to="/counter"><img className='home__image' src='https://cs8.pikabu.ru/avatars/1042/x1042055-1251797265.png' alt='tictactoe'></img>TicTacToe</Link>
+                <Link className='home__link' to="/counter"><img className='home__image' src='http://assets.stickpng.com/thumbs/5e9d98460ac2570004498cca.png' alt='hangman'></img>Hangman</Link>
+                <Link className='home__link' to="/counter"><img className='home__image' src='https://www.ajedrezeureka.com/wp-content/uploads/thumbs/custom/S/sudoku.png' alt='sudoku'></img>Sudoku</Link>
+            </div>
+        </div>
+    );
+};
